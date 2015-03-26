f1a314ab7bd71235e4d3103fff7b08f40d04f35e
Nurzhan Saktaganov <nur92world@mail.ru>
2015-03-27 02:15:04 +0300

    +compressing

--- hw2/reducer.py
+++ hw2/reducer.py
@@ -1,31 +1,40 @@
 #!/usr/bin/env python
 
 import sys
+import Simple9
+import VarByte
+import base64
 
 def main():
 
     previous_term = None
     list_of_doc_id = []
+    encoder = VarByte.VarByte
 
     for line in sys.stdin:
         current_term, doc_id = line.split('\t')
         if current_term == previous_term or previous_term == None:
             list_of_doc_id.append(int(doc_id))
         else:
-            print_result(previous_term, list_of_doc_id)
+            print_result(previous_term, list_of_doc_id,encoder)
             list_of_doc_id = [int(doc_id)]
 
         previous_term = current_term
 
-    print_result(previous_term, list_of_doc_id)
+    print_result(previous_term, list_of_doc_id,encoder)
 
 
 
-def print_result(term, list_of_doc_id):
-    sys.stdout.write(term)
-    for doc_id in list_of_doc_id:
-        sys.stdout.write('\t' + str(doc_id))
-    sys.stdout.write('\n')
+def print_result(term, list_of_doc_id,encoder):
+    encoded_byte_list = encoder.encode(list_of_doc_id)
+
+    output = ''
+    for byte in encoded_byte_list:
+        output += byte
+
+    output = base64.b64encode(output)
+
+    sys.stdout.write(term + '\t'+ output + '\n')
 
 
 if __name__ == '__main__':
--- hw2/run.sh
+++ hw2/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-
+echo '$> cat ./1_1000/docs-000.txt | python mapper.py | sort -s -k1,1 | python reducer.py > index.txt'
+cat ./1_1000/docs-000.txt | python mapper.py | sort -s -k1,1 | python reducer.py > index.txt
