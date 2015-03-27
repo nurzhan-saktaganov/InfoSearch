a0f9a2ec48ed5077916a4b31a65ef75cb42506fd
Nurzhan Saktaganov <nur92world@mail.ru>
2015-03-27 03:13:04 +0300

    modified:   hw2/run.sh

--- hw2/run.sh
+++ hw2/run.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+# lenta.ru povarenok.ru zr.ru
+pr1='lenta.ru'
+pr2='povarenok.ru'
+pr3='zr.ru'
 
-echo '$> cat ./1_1000/docs-000.txt | python mapper.py | sort -s -k1,1 | python reducer.py > index.txt'
-cat ./1_1000/docs-000.txt | python mapper.py | sort -s -k1,1 | python reducer.py > index.txt
+project=$pr3
+mkdir index
+echo '$> cat ./1_1000/'$project'/docs-000.txt | python mapper.py | sort -s -k1,1 | python reducer.py > index/'$project'.txt'
+cat ./1_1000/${project}/docs-000.txt | python mapper.py | sort -s -k1,1 | python reducer.py > index/${project}.txt
+rm VarByte.pyc Simple9.pyc
+
