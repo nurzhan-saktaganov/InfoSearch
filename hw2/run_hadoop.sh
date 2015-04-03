fbb4ad130bc151571be7ef6ccbe8747038833b86
Nurzhan Saktaganov <nur92world@mail.ru>
2015-04-03 21:54:24 +0300

    -up

--- hw2/run_hadoop.sh
+++ hw2/run_hadoop.sh
@@ -1,8 +1,16 @@
+#!/bin/sh
+#rm -r hw2_files
+mkdir hw2_files
+cp mapper.py hw2_files/
+cp reducer.py hw2_files/
+cp VarByte.py hw2_files/
+cp Simple9.py hw2_files/
+
 hadoop fs -rm -r hw2_index
 
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
-	 -file mapper.py reducer.py  VarByte.py Simple9.py\
-	 -mapper mapper.py \
-	 -reducer reducer.py \
-	 -input  /data/sites/lenta.ru/1_10/docs-000.txt \
+	 -file ./hw2_files \
+	 -mapper ./hw2_files/mapper.py \
+	 -reducer ./hw2_files/reducer.py \
+	 -input  /data/sites/lenta.ru/all/docs* \
 	 -output hw2_index
