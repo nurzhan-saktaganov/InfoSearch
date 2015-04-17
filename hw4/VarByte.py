970b150f4b4de2639671f8dbbf17da2285abcc02
Nurzhan Saktaganov <nur92world@mail.ru>
2015-04-17 15:10:19 +0300

    mapper + reducer are rewrited

---
+++ hw4/VarByte.py
@@ -0,0 +1,52 @@
+import base64
+
+class VarByte:
+    @staticmethod
+    def encode(input_numbers,to_diff=True):
+        if to_diff:
+            numbers = VarByte.__to_diff_representation(input_numbers)
+        else:
+            numbers = input_numbers
+
+        result = []
+        for number in numbers:
+            number_bytes = []
+            while number > 127:
+                number_bytes.append(chr(number % 128))
+                number /= 128
+            number_bytes.append(chr(number))
+            number_bytes[0] = chr(ord(number_bytes[0]) + 128)
+            number_bytes.reverse()
+            result += number_bytes
+        return base64.b64encode(str(bytearray(result)))
+
+    @staticmethod
+    def decode(input_byte_list,from_diff=True):
+        number, result = 0, []
+        byte_list = map(ord, input_byte_list)
+        for byte in byte_list:
+            number *= 128
+            number += (byte % 128)
+            if byte > 127:
+                result.append(number)
+                number = 0
+        if from_diff:
+            result = VarByte.__from_diff_representation(result)
+
+        return result
+
+    @staticmethod
+    def __to_diff_representation(numbers):
+        result = [ numbers[len(numbers) - i - 1] - numbers[len(numbers) - i - 2]\
+                        for i in range(len(numbers) - 1)]
+        result.append(numbers[0])
+        result.reverse()
+        return result
+
+    @staticmethod
+    def __from_diff_representation(numbers):
+        summ, result = 0, []
+        for number in numbers:
+            summ += number
+            result.append(summ)
+        return result
---
+++ hw4/reducer.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import Simple9
+import VarByte
+import base64
+
+import collections
+
+def main():
+
+    previous_term = None
+    dictionary = {}
+
+    encoder = VarByte.VarByte
+
+    if len(sys.argv) == 2 and sys.argv[1] == 'Simple9':
+    	encoder = Simple9.Simple9
+
+    for line in sys.stdin:
+        current_term, doc_id, count, positions = line[:-1].split('\t')
+        if current_term == previous_term or previous_term == None:
+            dictionary[int(doc_id)] = [int(count), map(int, positions.split(','))]
+        else:
+            print_result(term=previous_term, dictionary=dictionary, encoder=encoder)
+            dictionary = {}
+            dictionary[int(doc_id)] = [int(count), map(int, positions.split(','))]
+
+        previous_term = current_term
+
+    print_result(term=previous_term, dictionary=dictionary, encoder=encoder)
+
+
+#output format: <term><\tab><document frequency><\tab><list of doc_id><\tab>
+#                    <list of counts><\tab><comma separated list of positions list>
+def print_result(term, dictionary, encoder):
+    ordered_dictionary = collections.OrderedDict(sorted(dictionary.items()))
+    list_of_doc_id, list_of_counts, list_of_positions_list = [], [], []
+
+    for key, value in ordered_dictionary.iteritems():
+        list_of_doc_id.append(key)
+        list_of_counts.append(value[0])
+        list_of_positions_list.append(value[1])
+
+    encoded_list_of_doc_id = encoder.encode(list_of_doc_id, True)
+    encoded_list_of_counts = encoder.encode(list_of_counts, False)
+    encoded_list_of_positions_list = map(encoder.encode, list_of_positions_list)
+
+    output = term.decode('utf-8') + '\t' + str(len(list_of_doc_id)) + '\t'\
+                 + encoded_list_of_doc_id + '\t'\
+                 + encoded_list_of_counts + '\t'\
+                 + ','.join(encoded_list_of_positions_list)
+
+    print output.encode('utf-8')
+
+if __name__ == '__main__':
+    main()
