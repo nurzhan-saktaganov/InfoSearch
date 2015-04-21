b2b2fab8011fc5bc63a1cdd18ca59dd2e1f3dadf
Nurzhan Saktaganov <nur92world@mail.ru>
2015-04-22 02:53:35 +0300

    filter_marks.py

---
+++ hw4/filter_marks.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import argparse
+
+__author__ = 'Nurzhan Saktaganov'
+
+# -m / --marks - <file with marks>
+# -u / --urls - <file with urls>
+# -o / --output - <output, filtered marks>
+    
+def get_args():
+    parser = argparse.ArgumentParser(\
+        description='Filter assessors\' marks by urls', epilog='by ' +__author__)
+    parser.add_argument('-m', '--marks', help='assessors\' marks', metavar='<file with marks>',\
+        dest='marks', required=True, type=str)
+    parser.add_argument('-u', '--urls', help='urls file', metavar='<file with urls>',\
+        dest='urls', required=True, type=str)
+    parser.add_argument('-o', '--output', help='output, filtered marks', metavar='<output>',\
+        dest='output', required=True, type=str, default='output.marks')
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+
+    get_url = lambda line: line.strip().split('\t')[1].split('?')[0]
+    normalize = lambda url: url[:-1] if url[-1] == '/' else url
+
+    with open(args.urls, 'r') as f:
+        url_filter = [normalize(get_url(line)) for line in f]
+   
+    output = open(args.output, 'w')
+    with open(args.marks, 'r') as f:
+        for line in f:
+            if line.strip().split('\t')[1] in url_filter:
+                output.write(line)
+    output.close()
+    
+if __name__ == '__main__':
+    main()
