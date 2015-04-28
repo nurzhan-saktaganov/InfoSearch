8483ee5097ea97af40c015ccb815b5dae2d4213b
Nurzhan Saktaganov <nur92world@mail.ru>
2015-04-28 18:31:38 +0300

    reducer.py is optimized

--- hw4/reducer.py
+++ hw4/reducer.py
@@ -6,65 +6,48 @@ import Simple9
 import VarByte
 import base64
 
-import collections
-
-def read_data(input_file):
-	for line in input_file:
-		yield line
-
+#global variables
 
 def main():
 
     previous_term = None
-    dictionary = {}
+    list_of_docs = []
 
     encoder = VarByte.VarByte
 
     if len(sys.argv) == 2 and sys.argv[1] == 'Simple9':
-    	encoder = Simple9.Simple9
+        encoder = Simple9.Simple9
 
-    data = read_data(sys.stdin)
-
-    for line in data:
-        current_term, doc_id, count, positions = line[:-1].split('\t')
+    for line in sys.stdin:
+        current_term, doc_id, count, positions = line.strip().split('\t')
         if current_term == previous_term or previous_term == None:
-            dictionary[int(doc_id)] = [int(count), map(int, positions.split(','))]
+            list_of_docs.append([int(doc_id), int(count), map(int, positions.split(','))])
         else:
-            print_result(term=previous_term, dictionary=dictionary, encoder=encoder)
-            dictionary = {}
-            dictionary[int(doc_id)] = [int(count), map(int, positions.split(','))]
+            print_result(term=previous_term, list_of_docs=list_of_docs, encoder=encoder)
+            list_of_docs = []
+            list_of_docs.append([int(doc_id), int(count), map(int, positions.split(','))])
 
         previous_term = current_term
 
-    print_result(term=previous_term, dictionary=dictionary, encoder=encoder)
+    print_result(term=previous_term, list_of_docs=list_of_docs,encoder=encoder)
 
 
 #output format: <term><\tab><document frequency><\tab><list of doc_id><\tab>
 #                    <list of counts><\tab><comma separated list of positions list>
-def print_result(term, dictionary, encoder):
-    # dictionary sorted by key
-    ordered_dictionary = collections.OrderedDict(sorted(dictionary.items(), key=lambda t: t[0]))
-    list_of_doc_id, list_of_counts, list_of_positions_list = [], [], []
+def print_result(term, list_of_docs,encoder):
+    # sort by doc id
+    list_of_docs.sort(key=lambda value: value[0])
 
-    for key, value in ordered_dictionary.iteritems():
-        list_of_doc_id.append(key)
-        list_of_counts.append(value[0])
-        list_of_positions_list.append(value[1])
+    encoded_list_of_doc_id = encoder.encode([list_of_docs[i][0] for i in range(len(list_of_docs))], True)
+    encoded_list_of_counts = encoder.encode([list_of_docs[i][1] for i in range(len(list_of_docs))], False)
+    encoded_list_of_positions_list = map(encoder.encode, [list_of_docs[i][2] for i in range(len(list_of_docs))])
 
-    encoded_list_of_doc_id = encoder.encode(list_of_doc_id, True)
-    encoded_list_of_counts = encoder.encode(list_of_counts, False)
-    encoded_list_of_positions_list = map(encoder.encode, list_of_positions_list)
+    output = term.decode('utf-8') + '\t' + str(len(list_of_docs)) + '\t' \
+                + encoded_list_of_doc_id + '\t'\
+                + encoded_list_of_counts + '\t' \
+                + ','.join(encoded_list_of_positions_list)
 
-    output = term.decode('utf-8') + '\t' + str(len(list_of_doc_id)) + '\t'\
-                 + encoded_list_of_doc_id + '\t'\
-                 + encoded_list_of_counts + '\t'\
-
-    for encoded_list_of_positions in encoded_list_of_positions_list:
-        output +=  encoded_list_of_positions + ','
-
-    output = output[:-1] + '\n'
-
-    sys.stdout.write(output.encode('utf-8'))
+    print output.encode('utf-8')
 
 if __name__ == '__main__':
     main()
