83dc9823103e69a192a247006bfff8d165cf31f9
nurzhan25 <nur92world@mail.ru>
2015-05-18 14:54:24 -0700

    Create hints_run.sh

---
+++ hw5/HINTS/hints_run.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+INPUT='./hw5_raw_graph'
+OUTPUT='./hw5_HINTS_Step_0'
+
+hadoop fs -rm -r ${OUTPUT}
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+	-file init_mapper.py init_reducer.py \
+	-mapper init_mapper.py \
+	-reducer init_reducer.py \
+	-input ${INPUT} \
+	-output ${OUTPUT} \
+	-numReduceTasks 8
+
+for ((i=1;i<=5;i++))
+do
+	INPUT=${OUTPUT}
+	OUTPUT='./hw5_HINTS_Step_'${i}
+
+	hadoop fs -rm -r ${OUTPUT}
+	hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+		-file mapper.py reducer.py \
+		-mapper mapper.py \
+		-reducer reducer.py \
+		-input ${INPUT} \
+		-output ${OUTPUT} \
+		-numReduceTasks 8
+
+	hadoop fs -rm -r ${INPUT}
+done
