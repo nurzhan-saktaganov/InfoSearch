301b22fa5f0cf7cfa1a064baa0370e1a389e889b
nurzhan25 <nur92world@mail.ru>
2015-05-18 14:32:54 -0700

    Update mapper.py

--- hw5/PageRank/mapper.py
+++ hw5/PageRank/mapper.py
@@ -9,32 +9,24 @@ D = 0.85
 
 def main():
 
-    previous_url, adjacency_list, page_rank = None, '{}', 0.0
-
     for line in sys.stdin:
         # remove last \n
-        splitted_line = line[:-1].decode('utf-8').split('\t')
+        line = line[:-1]
+        
+        # send structure
+        print line
 
-        current_url = splitted_line[0]
+        current_url, page_rank, adjacency_list = line.decode('utf-8').split('\t')
 
-        if current_url != previous_url and previous_url != None:
-            print_result(previous_url, (1.0 - D) + D * page_rank, adjacency_list)
-            adjacency_list = '{}'
-            page_rank = 0.0
+        if adjacency_list == '{}':
+            continue
 
-        # if structure
-        if len(splitted_line) == 3:
-            adjacency_list = splitted_line[2]
-        # else
-        elif len(splitted_line) == 2:
-            page_rank += float(splitted_line[1])
+        adjacency_list = adjacency_list[1:-1].split(',')
 
-        previous_url = current_url
+        p = float(page_rank) / len(adjacency_list)
 
-    print_result(previous_url, (1.0 - D) + D * page_rank, adjacency_list)
-
-def print_result(url, page_rank, adjacency_list):
-    print ('%s\t%lf\t%s' % (url, page_rank, adjacency_list)).encode('utf-8')
+        for url in adjacency_list:
+            print ('%s\t%lf' % (url, p)).encode('utf-8')
 
 if __name__ == '__main__':
     main()
