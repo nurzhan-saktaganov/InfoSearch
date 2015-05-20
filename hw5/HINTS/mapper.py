95bdfa3ae8e59a0a5a9d229ed0f363aec67a96c7
nurzhan25 <nur92world@mail.ru>
2015-05-20 09:38:02 -0700

    Update mapper.py

--- hw5/HINTS/mapper.py
+++ hw5/HINTS/mapper.py
@@ -5,6 +5,8 @@ import sys
 
 __author__ = 'Nurzhan Saktaganov'
 
+URL_SEPARATOR = ' '
+
 def main():
 
     for line in sys.stdin:
@@ -20,12 +22,12 @@ def main():
         hub = int(hub)
 
         if to_list != '{}':
-            for url in to_list[1:-1].split(','):
+            for url in to_list[1:-1].split(URL_SEPARATOR):
                 # our hub is other's authority
                 print ('%s\tA\t%d' % (url, hub)).encode('utf-8')
 
         if from_list != '{}':
-            for url in from_list[1:-1].split(','):
+            for url in from_list[1:-1].split(URL_SEPARATOR):
                 # out authority is other's hub
                 print ('%s\tH\t%d' % (url, authority)).encode('utf-8')
 
