894dd2b6d0675c89d3bb83e60224f17a69d05335
nurzhan25 <nur92world@mail.ru>
2015-05-20 09:36:33 -0700

    Update init_mapper.py

--- hw5/HINTS/init_mapper.py
+++ hw5/HINTS/init_mapper.py
@@ -5,15 +5,21 @@ import sys
 
 __author__ = 'Nurzhan Saktaganov'
 
+URL_SEPARATOR = ' '
+
 def main():
 
     for line in sys.stdin:
-    	# remove last \n
+        # remove last \n
         current_url, url_list = line[:-1].decode('utf-8').split('\t')
 
-        print ('%s\tTO\t{%s}' % (current_url, url_list)).encode('utf-8')
+        if url_list != ' ':
+            print ('%s\tTO\t{%s}' % (current_url, url_list)).encode('utf-8')
+        else:
+            print ('%s\tTO\t{}' % (current_url)).encode('utf-8')
+            continue
 
-        url_list = url_list.split(',')
+        url_list = url_list.split(URL_SEPARATOR)
 
         for url in url_list:
             print ('%s\tFROM\t%s' % (url, current_url)).encode('utf-8')
